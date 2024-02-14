1743b33437e83ec14640e2294727b7d08d8b434d
DeikunAleksandr <160006268+DeikunAleksandr@users.noreply.github.com>
2024-02-14 19:23:09 +1000

    lab1.c

--- lab1.c
+++ lab1.c
@@ -1,21 +1,23 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 int main() {
     int a, b, c, Res;
-    char continueFlag = 'y'; 
-    while (continueFlag == 'y') 
+    char continueFlag = 'y';
+    
+    do {
         printf("Input numbers:\n");
         scanf("%d", &a);
         scanf("%d", &b);
         scanf("%d", &c);
+        
         double x1, x2;
         Res = b * b - 4 * a * c;
         printf("Diskrimenant: %d\n", Res);
+        
         if (Res < 0)
-            printf("Korney net");
+            printf("Korney net\n");
         else if (Res == 0) {
             x1 = (-b - sqrt(Res)) / (2 * a);
             printf("x1=%.2lf\n", x1);
@@ -24,9 +26,10 @@ int main() {
             x2 = (-b + sqrt(Res)) / (2 * a);
             printf("x1=%.2lf\nx2=%.2lf\n", x1, x2);
         }
-        printf("Do you want to continue? (y/n): ");
-        scanf(" %c", &continueFlag); 
-        printf("\n");
-    }
+        
+        printf("Continue? (y/n): ");
+        scanf(" %c", &continueFlag);
+    } while (continueFlag == 'y');
+    
     return 0;
 }
